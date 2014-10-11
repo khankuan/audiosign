490394af0dd02db6b65aeecd87b3b200cca6c5df
khankuan <khankuan@gmail.com>
2014-10-11 16:38:47 +0800

    Changed to use hex id, used dsp.js

--- gruntfile.js
+++ gruntfile.js
@@ -14,6 +14,7 @@ module.exports = function(grunt){
             },
             dist: {
 				src: [
+					'lib/dsp.js',
 		            'lib/util.js',
 		            'lib/audiosignbroadcaster.js',
 		            'lib/audiosignlistener.js',
